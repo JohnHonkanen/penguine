1d281d52aac12cd3ed1109b28b7dac0a2fa5401e
Lapalm <anthony_palm@hotmail.co.jp>
2017-03-06 23:29:29 +0000

    Re-added the main

---
+++ Engine/main.cpp
@@ -0,0 +1,25 @@
+#include "SDLRenderer.h"
+#include "GLFWRenderer.h"
+#include "BallGenerator.h"
+#include "Shoot.h"
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+	GLFWRenderer renderer;
+	renderer.init();
+	BallGenerator bg(vec3(0,0,0), vec3(0,500,0));
+	bg.init();
+	while (true) {
+
+		//bg.update(0.01f);
+		renderer.start();
+		renderer.drawVerts();
+		//bg.render(&renderer);
+		//Do something in main
+		renderer.end();
+	}
+	renderer.destroy();
+	return 0;
+}
