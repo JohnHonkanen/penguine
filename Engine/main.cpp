9f5071ae431da2979cf79af1ab38b2e40dfc0620
Chris <jc.honkanen@gmail.com>
2017-03-12 13:54:16 +0000

    Integrated Clock Loop into Main

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,21 +1,61 @@
 #include <iostream>
+
+#include "TextureManager.h"
+#include "Shader.h"
+#include "Window.h"
+#include "glfwWindow.h"
+#include "openGLRenderer.h"
+#include "BallGenerator.h"
 #include "Clock.h"
 
+
 using namespace std;
+using namespace glm;
 
 int main(int argc, char *argv[])
 {
-	Clock clock;
-	clock.startClock();
-	clock.setDelay(5000);
+
+	Window *window = new glfwWindow(800, 600);
+	openGLRenderer renderer(window);
+	renderer.init(); // Initialize Rendering Library
+
+	TextureManager textureManager;
+	textureManager.saveTexture("awesomeface.png", "container");
+
+	Shader shaderProgram("minimal.vert", "single.frag"); // Initialize Shader Programs
+
+	BallGenerator ballGen(vec3(0.0f), vec3(0.0f, 15.0f, 0.0f), &textureManager, &shaderProgram);
+	ballGen.init();
+
+	Clock appClock;
+	appClock.startClock();
+	Clock frameClock;
+	frameClock.startClock();
+	frameClock.setDelay(1000.0f/60); // 60FPS
+	
+	double previousTime = 0.0f;
+	double currentTime = 0.0f;
 	bool flag = true;
-	while (flag) {
-		clock.updateClock();
-		cout << clock.getMilliseconds() << endl;
-		if (clock.alarm()) {
-			cout << "5 Seconds has passed" << endl;
-			flag = false;
+	while(flag) {
+
+		frameClock.updateClock(); // Ticks our Frame Clock
+		appClock.updateClock(); //Ticks App Clock
+		// Calculates Delta Time
+		currentTime = appClock.getMilliseconds();
+		double dt = (currentTime - previousTime) * 0.001f;
+		previousTime = currentTime;
+		//End of DeltaTime
+		ballGen.update(dt); //updates balls
+		if (frameClock.alarm()) {
+			renderer.start();  // Sets up Rendering Loop
+			ballGen.render(&renderer); // Renders Balls
+			renderer.end(); //Swap Buffers
+			frameClock.resetClock(); // Once frame is done reset to 0
 		}
 	}
+
+	cin.get();
+	renderer.destroy();
+
 	return 0;
 }
