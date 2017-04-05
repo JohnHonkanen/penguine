0da6c9db38bff20aad27dd82704c5ef48cf8a35d
Lapalm <anthony_palm@hotmail.co.jp>
2017-04-05 22:52:51 +0100

    added in check collision and handlecollision. Which needs updating.

--- Engine/CollisionManager.cpp
+++ Engine/CollisionManager.cpp
@@ -1,5 +1,6 @@
 #include "CollisionManager.h"
 #include "CollisionAABB.h"
+#include "Utils.h"
 
 CollisionManager *CollisionManager::manager = nullptr;
 
@@ -40,10 +41,44 @@ bool CollisionManager::checkCollision(CollisionAABB a, CollisionAABB b)
 
 void CollisionManager::handleCollision(CollisionAABB * a, CollisionAABB * b)
 {
+	a->colliding = true;
+	b->colliding = true;
+
+	vec3 box1Min = a->position - a->halfExtents;
+	vec3 box1Max = a->position + a->halfExtents;
+
+	vec3 box2Min = b->position - b->halfExtents;
+	vec3 box2Max = b->position + b->halfExtents;
+
+	vec3 normal = Utils::getAABBFaceNormal(a->position, b);
+
+	a->contactNormal = normal;
+	b->contactNormal = -normal;
+	vec3 norm = normal;
+	
 }
 
 void CollisionManager::update()
 {
+	for (int i = 0; i < colObjects.size(); i++) {
+		CollisionAABB *iObjs = colObjects.at(i);
+		//GameObject *object = iObjs->gameObject;
+
+		iObjs->colliding = false;
+		//iObjs->position = vec3(object->transform.physics[3]) + iObjs->offset;
+
+	}
+
+	//To Be filled in
+	for (int i = 0; i < colObjects.size(); i++) {
+		CollisionAABB *iObjs = colObjects.at(i);
+		for (int k = i + 1; k < colObjects.size(); k++) {
+			CollisionAABB *kObjs = colObjects.at(k);
+
+			if (checkCollision(*iObjs, *kObjs))
+				handleCollision(iObjs, kObjs);
+		}
+	}
 }
 
 
